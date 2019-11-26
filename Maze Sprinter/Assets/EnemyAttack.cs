402f10d4e2d000361153271a605a5142820bdc6d
duduVictor <53229178+duduVictor@users.noreply.github.com>
2019-11-26 16:24:55 -0600

    with enemy
    Maze sprinter with the enemy

---
+++ Maze Sprinter/Assets/EnemyAttack.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject == player)
+        {
+
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == player)
+        {
+
+            playerInRange = false;
+        }
+    }
+
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+
+        if (timer >= timeBetweenAttacks && playerInRange)
+        {
+            Attack();
+        }
+
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+
+    void Attack()
+    {
+
+        timer = 0f;
+
+
+        if (playerHealth.currentHealth > 0)
+        {
+
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Maze Sprinter/Assets/PlayerHealth.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public Image damageImage;
+
+
+
+    FirstPersonAIO playerController;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+
+        playerController = GetComponent<FirstPersonAIO>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+            
+            if (currentHealth <= 0 && !isDead)
+            {
+
+            Death();
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+        playerController.enabled = false;
+    }
+}
+
